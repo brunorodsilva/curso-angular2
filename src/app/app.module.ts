3fd3bf44e00d9c1ab432fa67a977b8fb5cc19749
brunorodsilva <brunord2014@hotmail.com>
2023-02-03 18:20:27 -0300

    add: Aula 5 - Módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CursosModule } from './cursos/cursos.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -6,7 +7,6 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,6 +17,7 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
