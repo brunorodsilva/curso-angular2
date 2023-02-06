5618b01ab0d0fd76ba97cce6499316656c886b18
brunorodsilva <brunord2014@hotmail.com>
2023-02-05 21:04:37 -0300

    add: Aula 07 - Services e Injeção de dependência

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -8,10 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class CursosComponent implements OnInit {
 
   nomePortal: string;
-  cursos: string[] = ['Java', 'Ext JS', 'Angular']
-
-  constructor() {
+  cursos: string[];
+  constructor(private cursosService: CursosService) {
     this.nomePortal = 'http://loiane.training';
+    // var servico = new CursosService();
+    this.cursos = this.cursosService.getCursos();
   }
 
   ngOnInit() {
